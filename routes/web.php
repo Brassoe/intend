44ae61361585a048cbd234d35ddbf88f9ebac4d1
Philip Grand <1067829@ucn.dk>
2019-11-20 12:26:40 +0100

    Added new routes (some dummy routes)

--- routes/web.php
+++ routes/web.php
@@ -15,32 +15,99 @@ use Illuminate\Support\Str;
 |
 */
 
+// default route
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->get('/hello', function () use ($router) {
-    return 'hello world';
-});
-
-$router->get('/db', function () use ($router) {
-    return app('db')->table('migrations')->get();
-});
-
+// catalog (excluding modules already installed by user)
 $router->get('/catalog', function () use ($router) {
-	$ids = DB::table('modules_users')->where('fk_user', '=', $_GET['uid'])->pluck('fk_module');
+	// pluck list of module IDs belonging to user
+	$ids = DB::table('modules_users')->where('fk_user', '=', $_SERVER['HTTP_UID'])->pluck('fk_module');
+
+	// get list of modules not belonging to the user
 	$result = DB::table('modules')->whereNotIn('id', $ids->all())->get();
+
+	// add dummy images for test purposes - TODO: remove
 	foreach($result as &$item) {
 		$item->images = [
 			"https:/placebeard.it/256/256/notag?random=".Str::random(3),
 			"https:/placebeard.it/256/256/notag?random=".Str::random(3)
 		];
 	}
+
 	return $result;
 });
 
+$router->get('/catalog/install/{slug}', function ($slug) use ($router) {
+	// TODO: should associate the given module with the user
+	$module = DB::table('modules')->where('name', '=', $slug)->first();
+
+	if($module === null) {
+		return new JsonResponse(false, 404);
+	}
+
+	try {
+		$result = DB::table('modules_users')->insert([
+			'fk_module' => $module->id,
+			'fk_user' => $_SERVER['HTTP_UID']
+		]);
+	} catch (PDOException $e) {
+		$result = false;
+	}
+
+	return new JsonResponse($result, $result ? 200 : 404);
+});
+
+$router->get('/module/{slug}/{action}', function ($slug, $action) use ($router) {
+	// TODO: this is the part that modules should be able to register
+});
+
+$router->delete('/modules/delete/{slug}', function ($slug) use ($router) {
+	// TODO: delete the association between the given module and the user
+	$module = DB::table('modules')->where('name', '=', $slug)->first();
+
+	if($module === null) {
+		return new JsonResponse(false, 404);
+	}
+
+	$result = !!DB::table('modules_users')->where('fk_module', '=', $module->id)->where('fk_user', '=', $_SERVER['HTTP_UID'])->delete();
+
+	return new JsonResponse($result, $result ? 200 : 404);
+});
+
+$router->post('/user/create', function () use ($router) {
+	// TODO: validate and create the user (this should probably be a POST request)
+	// Required fields:
+	//	id
+	//	name
+	//	email
+	//	address
+	//	profile_img (TODO: this probably shouldn't be required)
+});
+
+$router->delete('/user/delete', function () use ($router) {
+	// TODO: delete the user (none of this new-fangled CRU, we go full CRUD)
+	// TODO: check that this actually works
+	$result = DB::table('users')->delete($_SERVER['HTTP_UID']) > 0;
+
+	return new JsonResponse($result, $result ? 200 : 404);
+});
+
+$router->put('/user/update', function () use ($router) {
+	// TODO: update the user (this should probably be a PUT request)
+});
+
+$router->get('/user', function () use ($router) {
+	return new JsonResponse(DB::table('users')->find($_SERVER['HTTP_UID']));
+});
+
+// user's modules
 $router->get('/modules', function () use ($router) {
-	$result = DB::table('modules')->join('modules_users', 'modules.id', '=', 'modules_users.fk_module')->where('modules_users.fk_user', '=', $_GET['uid'])->get();
+	// get list of modules belonging to user
+	$result = DB::table('modules')->join('modules_users', 'modules.id', '=', 'modules_users.fk_module')->where('modules_users.fk_user', '=', $_SERVER['HTTP_UID'])->get();
+
+	// add dummy images for test purposes - TODO: remove
 	foreach($result as &$item) {
 		$item->images = [
 			"https:/placebeard.it/256/256/notag?random=".Str::random(3),
@@ -50,20 +117,22 @@ $router->get('/modules', function () use ($router) {
 	return $result;
 });
 
-$router->get('/modules/{id}', function ($id) use ($router) {
-	$result = DB::table('modules')->find($id);
+$router->get('/modules/{slug}', function ($slug) use ($router) {
+	// find specific module
+	$result = DB::table('modules')->where('name', '=', $slug)->first();
+	$id = $result->id;
 
+	// add dummy images for test purposes - TODO: remove
 	if(!empty($result)) {
 		$result->images = [
 			//"https://unsplash.it/256/256?random",
 			"https:/placebeard.it/256/256/notag?random=".Str::random(3),
 			"https:/placebeard.it/256/256/notag?random=".Str::random(3)
 		];
+
+		// add a field describing whether the user has this module installed or not
+		$result->installed = !!DB::table('modules_users')->where('fk_user', '=', $_SERVER['HTTP_UID'])->where('fk_module', '=', $id)->selectRaw('IF(COUNT(*) > 0, 1, 0) AS installed')->first()->installed;
 	}
 
 	return new JsonResponse($result);
-});
-
-$router->get('/users/{id}', function ($id) use ($router) {
-	return new JsonResponse(DB::table('users')->find($id));
 });
