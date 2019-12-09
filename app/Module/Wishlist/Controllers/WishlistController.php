a909730304fbf41488fa8fa9bdf3c2eb4cae6183
Philip Grand <1067829@ucn.dk>
2019-12-09 12:56:06 +0100

    Added Wishlist module stub

---
+++ app/Module/Wishlist/Controllers/WishlistController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Module\Wishlist\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Laravel\Lumen\Routing\Router;
+use App\Http\Controllers\Controller;
+use App\Module\ModuleInterface;
+use App\Module\CaseJuggler;
+use App\Model\Module;
+
+class WishlistController extends Controller implements ModuleInterface {
+	public function install() { // required by interface
+		// run migrations
+		if(!is_dir(__DIR__.'/../../../../public/modules/wishlist'))
+			mkdir(__DIR__.'/../../../../public/modules/wishlist', 0777, true);
+		copy(__DIR__.'/../icon.svg', __DIR__.'/../../../../public/modules/wishlist/icon.svg');
+		Module::insert([
+			'name' => 'wishlist',
+			'display_name' => 'Ønskeliste',
+			'description' => 'Holder styr på ønskelister',
+			'color' => '154, 194, 94',
+			'price' => 10000,
+			'category' => 'Planlægning',
+			'icon' => 'mdi-gift'
+		]);
+	}
+
+	public function uninstall() { // required by interface
+		// rollback migrations
+	}
+
+	public function route(Router $router) { // required by interface
+		// register routes
+		$router->group(['prefix' => '/wishlist'], function () use ($router) {
+			// lists
+			$router->get('/', function() {
+				return 'Ønskeliste';
+			});
+		});
+	}
+}
+
